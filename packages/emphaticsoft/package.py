fabf95b320235a7791c6155f23a8854560cf8e22
Gavin Davies <gsdavies@fnal.gov>
2026-02-01 06:51:54 -0600

    Update dependencies in emphaticsoft package.py for improved functionality. -  a few TODOs added to check certain dependencies - needed to add in more; based only on product_deps isn't enough

--- packages/emphaticsoft/package.py
+++ packages/emphaticsoft/package.py
@@ -30,17 +30,51 @@ class Emphaticsoft(CMakePackage):
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
+    depends_on("cetmodules", type="build")
+    depends_on("cmake", type="build")
 
+    # Boost with specific components required by emphaticsoft
+    # Components needed: iostreams, math, serialization
+    # TODO: Verify if all these components are needed
+    depends_on("boost+iostreams+math+serialization")
+
+    # art dependencies
     depends_on("art")
     depends_on("art_root_io")
+    depends_on("canvas_root_io")
+    depends_on("cetlib")
+    depends_on("cetlib-except")
+    depends_on("fhicl-cpp")
+    depends_on("messagefacility")
+
+    # DAQ dependencies
     depends_on("artdaq")
     depends_on("artdaq_core")
+
+    # ROOT with specific components required by emphaticsoft
+    # Components needed: 
+    # Core, EG, Eve, Geom, Gdml, Gpad, Graf, Graf3d,
+    # Gui, Hist, Matrix, MathCore, MathMore, Minuit
+    # Minuit2, Net, Physics, RIO, Spectrum, TMVA,
+    # Thread, Tree, TreePlayer, X3d, XMLIO
+    # TODO: Verify if all these components are needed
+    depends_on("root+gdml+minuit+spectrum+tmva+x+xml+threads")
+    # Simulation dependencies
     depends_on("geant4")
+
+    # Data Handline & Database dependencies
+    depends_on("ifdh_art")
     depends_on("ifbeam")
     depends_on("nucondb")
-    depends_on("ifdh_art")
-    depends_on("srproxy")
-    depends_on("cetmodules", type="build")
+
+    # Other dependencies
+    depends_on("milliepede-ii")
+    depends_on("libtorch")
+    depends_on("protobuf")
+    depends_on("nlohmann-json")
+    
+    # Python dependencies
+    depends_on("python")
 
     def setup_run_environment(self, env):
         prefix = self.prefix
